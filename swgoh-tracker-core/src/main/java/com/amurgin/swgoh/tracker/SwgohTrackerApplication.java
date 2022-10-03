71558cde351e39c937121e0d2edb40edc4eedbd6
Andrey Murgin <amurgin@natera.com>
2022-10-04 02:04:40 +0300

    Init - multi-module structure

---
+++ swgoh-tracker-core/src/main/java/com/amurgin/swgoh/tracker/SwgohTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.amurgin.swgoh.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwgohTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwgohTrackerApplication.class, args);
+	}
+
+}
---
+++ swgoh-tracker-core/src/test/java/com/amurgin/swgoh/tracker/SwgohTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amurgin.swgoh.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwgohTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
