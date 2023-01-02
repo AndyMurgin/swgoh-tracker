713d493616f2a2e82499e803ffc666095aa43e6e
Andrey Murgin <murgin.andy@gmail.com>
2023-01-02 15:38:33 +0300

    #2 - new module swgoh-tracker-accounts

---
+++ swgoh-tracker-accounts/src/main/java/com/amurgin/swgoh/tracker/accounts/SwgohTrackerAccountsApplication.java
@@ -0,0 +1,12 @@
+package com.amurgin.swgoh.tracker.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwgohTrackerAccountsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SwgohTrackerAccountsApplication.class, args);
+  }
+}
---
+++ swgoh-tracker-accounts/src/test/java/com/amurgin/swgoh/tracker/accounts/SwgohTrackerAccountsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.amurgin.swgoh.tracker.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwgohTrackerAccountsApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
